3f17bd6d32f6e5e575d9a66e871cf7ac7588ba51
Arca819 <101073133+Arca819@users.noreply.github.com>
2022-03-07 18:44:31 +0900

    Chapter 10 提出課題：FizzBuzz

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -3,17 +3,27 @@ public class FizzBuzz {
 
 	public static void main(String[] args) {
 		for (int i = 1; i <= 100; i++) {
-			int a = i % 3; // 3 で割った余りを a に代入する。
-			int b = i % 5; // 5 で割った余りを b に代入する。
+			// 3 で割った余りを a に代入
+			int a = i % 3;
+			// 5 で割った余りを b に代入
+			int b = i % 5;
 			// if文
-			if ((a == 0) && (b == 0)) { // i が 3 の倍数 かつ 5 の倍数のとき、
-				System.out.println("FizzBuzz"); // "FizzBuzz"を表示する
-			} else if ((a == 0) && (b != 0)) { // i が 3 の倍数 かつ 5 の倍数ではないとき、
-				System.out.println("Fizz"); // "Fizz"を表示する
-			} else if ((a != 0) && (b == 0)) { // i が 3 の倍数ではない かつ 5 の倍数のとき、
-				System.out.println("Buzz"); // "Buzz"を表示する
-			} else { // i が 3 の倍数 でも 5 の倍数でもないとき、
-				System.out.println(i); // i を表示する
+			// i が 3 の倍数 かつ 5 の倍数のとき
+			if ((a == 0) && (b == 0)) {
+				// "FizzBuzz"を表示する
+				System.out.println("FizzBuzz");
+			// i が 3 の倍数 かつ 5 の倍数ではないとき
+			} else if ((a == 0) && (b != 0)) {
+				// "Fizz"を表示する
+				System.out.println("Fizz");
+			// i が 3 の倍数ではない かつ 5 の倍数のとき
+			} else if ((a != 0) && (b == 0)) {
+				// "Buzz"を表示する
+				System.out.println("Buzz");
+			// i が 3 の倍数 でも 5 の倍数でもないとき
+			} else {
+				// i を表示する
+				System.out.println(i); 
 			}
 		}
 	}
