88e8197e35d0c8f8fd30367ded2a23c3d35b664b
Arca819 <101073133+Arca819@users.noreply.github.com>
2022-03-10 12:46:26 +0900

    Chapter 5 提出課題：コレクションについて学ぼう

---
+++ src/practice/Task.java
@@ -0,0 +1,22 @@
+package practice;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Task {
+	public LocalDate todoDate; // タスク実行日
+	public String todoTask; // タスク内容
+
+	
+	// コンストラクタ
+	public Task(String date, String todoTask) {
+		this.todoDate = convertToLocalDate(date, "yyyy年M月d日");
+		this.todoTask = todoTask;
+	}
+
+	// 文字列日付を LocalDate に変換
+	public static LocalDate convertToLocalDate(String date,String format) {
+		return LocalDate.parse(date, DateTimeFormatter.ofPattern(format));
+	}
+
+}
